12d963835957c7f32af847af3c6e62db180aee93
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-04-04 16:20:52 +0300

    change

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,10 @@
-const item = document.querySelectorAll(".item")
+const categories = document.querySelectorAll("li.item");
+console.log("Number of categories:", categories.length);
+categories.forEach((category) => {
+    const title = category.querySelector(".title-list").textContent;
+    const elements = category.querySelectorAll("ul > li");
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${elements.length}`);
+});
+
 
